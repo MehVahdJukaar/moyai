ebbe65bbba96657f340e4f074452e693b24adb6a
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-09-12 11:07:27 +0200

    1.21

--- neoforge/src/main/java/net/mehvahdjukaar/moyai/neoforge/MoyaiForge.java
+++ neoforge/src/main/java/net/mehvahdjukaar/moyai/neoforge/MoyaiForge.java
@@ -4,12 +4,6 @@ import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
 import net.mehvahdjukaar.moonlight.api.platform.RegHelper;
 import net.mehvahdjukaar.moyai.Moyai;
 import net.minecraft.world.item.FireworkRocketItem;
-import net.minecraftforge.client.FireworkShapeFactoryRegistry;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.level.NoteBlockEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 /**
  * Author: MehVahdJukaar
@@ -17,15 +11,16 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(Moyai.MOD_ID)
 public class MoyaiForge {
 
-    public static final FireworkRocketItem.Shape MOYAI_FIREWORK =FireworkRocketItem.Shape.create(
+    public static final FireworkRocketItem.Shape MOYAI_FIREWORK = FireworkRocketItem.Shape.create(
             "moyai", FireworkRocketItem.Shape.values().length, "moyai");
 
-    public MoyaiForge() {
+    public MoyaiForge(IEventBus bus) {
+        RegHelper.startRegisteringFor(bus);
         Moyai.commonInit();
-        MinecraftForge.EVENT_BUS.addListener(MoyaiForge::onNoteBlockPlayer);
+        Neoforge.EVENT_BUS.addListener(MoyaiForge::onNoteBlockPlayer);
 
-        PlatHelper.addCommonSetup(()->RegHelper.registerFireworkRecipe(MOYAI_FIREWORK, Moyai.MOYAI_ITEM.get()));
-        if(PlatHelper.getPhysicalSide().isClient()){
+        PlatHelper.addCommonSetup(() -> RegHelper.registerFireworkRecipe(MOYAI_FIREWORK, Moyai.MOYAI_ITEM.get()));
+        if (PlatHelper.getPhysicalSide().isClient()) {
             FireworkShapeFactoryRegistry.register(MOYAI_FIREWORK, MoyaiFireworkShape::create);
         }
     }
